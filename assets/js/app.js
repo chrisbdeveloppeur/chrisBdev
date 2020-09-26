93e60dcbaccd37b2854e0be1d74b18d01f79e333
K4gur0 <kenshin91cb@gmail.com>
2020-09-26 11:14:54 +0200

    MAJ DESKTOP downgrade SF5 => SF4.4

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import '../css/app.scss';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+const logo = require('../img/Chris B Dev - Logo - sans-fond.svg');
+const logo2 = require('../img/Chris B Dev - Logo - reduit.svg');
+const logo3 = require('../img/Chris B Dev - Logo - reduit - Black.svg');
+const logo4 = require('../img/Chris B Dev - Logo - sans-fond - Black.svg');
