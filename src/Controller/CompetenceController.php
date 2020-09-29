ee73f78cd073277b92cd6499136565b6cb3a7570
K4gur0 <kenshin91cb@gmail.com>
2020-09-29 23:19:34 +0200

    MAJ DESKTOP modification front compétence

---
+++ src/Controller/CompetenceController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\CompType;
+use App\Repository\CompRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompetenceController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("ajouter-competence", name="add_comp")
+     */
+    public function add_competence(Request $request)
+    {
+        $form = $this->createForm(CompType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $competence = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($competence);
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('sections/competences/add_comp.html.twig',[
+            'comp_form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("edit-competence-{id}", name="edit_comp")
+     */
+    public function edit_competence(Request $request, CompRepository $compRepository, $id)
+    {
+        $competence = $compRepository->find($id);
+        $form = $this->createForm(CompType::class, $competence);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $competence = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($competence);
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('sections/competences/edit_comp.html.twig',[
+            'comp_form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("del-competence-{id}", name="del_comp")
+     */
+    public function del_competence(CompRepository $compRepository, $id)
+    {
+        $competence = $compRepository->find($id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($competence);
+        $entityManager->flush();
+        return $this->redirectToRoute('home');
+
+    }
+}
