f96a4605f2281afe85ecce6fa99acc7f93e23f59
K4gur0 <kenshin91cb@gmail.com>
2020-11-02 12:00:20 +0100

    MAJ DESKTOP correctif img size for lightbox projects

--- migrations/Version20201102104211.php
+++ migrations/Version20201102104211.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201031170658 extends AbstractMigration
+final class Version20201102104211 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,11 +21,13 @@ final class Version20201031170658 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE admin ADD news TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE projet ADD img_1_name VARCHAR(255) DEFAULT NULL, ADD img_2_name VARCHAR(255) DEFAULT NULL, ADD img_3_name VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE admin DROP news');
+        $this->addSql('ALTER TABLE projet DROP img_1_name, DROP img_2_name, DROP img_3_name');
     }
 }
