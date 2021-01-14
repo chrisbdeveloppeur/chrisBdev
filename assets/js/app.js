e91b96233c3bf854f4905a3464023213f2235c99
chrisbdeveloppeur <kenshin91cb@gmail.com>
2021-01-14 14:52:50 +0100

    MAJ DESKTOP add fixtures + all avis page

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,5 +34,6 @@ const bg_space_user_illustration = require('../img/undraw_profile_details_f8b7.s
 const bg_space_user_illustration_2 = require('../img/undraw_account_490v.svg');
 const bg_change_user_password_illustration = require('../img/undraw_security_o890.svg');
 const unamed = require('../img/unnamed.jpg');
+const unamed_b_w = require('../img/unnamed.B_W.jpg');
 const top_secret = require('../img/secret-3037639_1920.svg');
 const no_img = require('../img/no-photo.svg');
